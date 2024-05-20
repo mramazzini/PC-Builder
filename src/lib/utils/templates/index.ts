456905e98cf3a8a915bc2a98705f3ef1b6c50318
mramazzini <mramazzini123@gmail.com>
2024-05-20 17:14:00 -0500

    dynamically generate path

--- src/lib/utils/templates/index.ts
+++ src/lib/utils/templates/index.ts
@@ -2,15 +2,10 @@
 import fs from "fs";
 import path from "path";
 export async function verifyEmail(verificationCode: string, email: string) {
-  const templatePath = path.join(
-    process.cwd(),
-    "src",
-    "lib",
-    "utils",
-    "templates",
-    "verifyEmail.html"
-  );
-
+  const templatePath = await findFile("verifyEmail.html", process.cwd());
+  if (!templatePath) {
+    throw new Error("Template not found");
+  }
   const template = fs.readFileSync(templatePath, "utf8");
   return template
     .replace("${verificationCode}", verificationCode)
@@ -18,15 +13,35 @@ export async function verifyEmail(verificationCode: string, email: string) {
 }
 
 export async function questionnaireEntryEmail(code: string) {
-  const templatePath = path.join(
-    process.cwd(),
-    "src",
-    "lib",
-    "utils",
-    "templates",
-    "questionnaireEntry.html"
-  );
-  console.log(process.cwd(), templatePath);
+  const templatePath = await findFile("questionnaireEntry.html", process.cwd());
+  if (!templatePath) {
+    throw new Error("Template not found");
+  }
+  console.log(await findFile("questionnaireEntry.html", process.cwd()));
   const template = fs.readFileSync(templatePath, "utf8");
   return template.replace("${code}", code);
+}
+
+export async function findFile(filename: string, directory = __dirname) {
+  function searchDir(dir: string) {
+    const files = fs.readdirSync(dir);
+
+    for (const file of files) {
+      const filePath = path.join(dir, file);
+      const stat = fs.statSync(filePath);
+
+      if (stat.isDirectory()) {
+        const temp: string = searchDir(filePath);
+        if (temp) {
+          return temp;
+        }
+      } else if (file === filename) {
+        console.log(`File found: ${filePath}`);
+        return filePath;
+      }
+    }
+    return "";
+  }
+
+  return searchDir(directory);
 }
